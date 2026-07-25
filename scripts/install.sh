f0ccdefad17dfba76ff170e29aa1426b9c47cae3
Henry Unite <unitehenry@gmail.com>
2026-07-25 06:11:04 +0000

    chore: install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,29 +4,29 @@ set -e
 
 apt-get update
 
-# # Install Docker
-# if ! command -v docker >/dev/null 2>&1; then
-#   sudo apt update
-#   sudo apt install -y ca-certificates curl
-#   sudo install -m 0755 -d /etc/apt/keyrings
-#   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-#   sudo chmod a+r /etc/apt/keyrings/docker.asc
-# 
-#   sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-#   Types: deb
-#   URIs: https://download.docker.com/linux/ubuntu
-#   Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-#   Components: stable
-#   Architectures: $(dpkg --print-architecture)
-#   Signed-By: /etc/apt/keyrings/docker.asc
-#   EOF
-# 
-#   sudo apt update
-# 
-#   sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# fi
+# Install Docker
+if ! command -v docker >/dev/null 2>&1; then
+  sudo apt update
+  sudo apt install -y ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Build Xpod
+  sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+  Types: deb
+  URIs: https://download.docker.com/linux/ubuntu
+  Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+  Components: stable
+  Architectures: $(dpkg --print-architecture)
+  Signed-By: /etc/apt/keyrings/docker.asc
+  EOF
+
+  sudo apt update
+
+  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
+# Build xpod
 apt-get install -y git
 
 rm -rf /var/lib/xpod && git clone https://github.com/unitehenry/xpod /var/lib/xpod
@@ -38,7 +38,7 @@ docker build -t xpod-chromium -f /var/lib/xpod/apps/chromium/Containerfile /var/
 docker network inspect amish >/dev/null 2>&1 || docker network create amish
 docker rm -f guacd guacamole xpod-chromium agent-browser-mcp 2>/dev/null || true
 
-# Build Agent Browser MCP
+# Build agent browser mcp
 git clone https://github.com/unitehenry/agent-browser-mcp /var/lib/agent-browser-mcp
 
 podman build -t agent-browser-mcp -f /var/lib/agent-browser-mcp/Containerfile /var/lib/agent-browser-mcp
@@ -49,8 +49,7 @@ docker run -d --network amish --name guacd docker.io/guacamole/guacd
 # Run guacamole
 mkdir -p /var/lib/guacamole/config
 
-# TODO: update download url
-curl https://github.com/unitehenry/amish/guacamole/config/user-mapping.xml -o /var/lib/guacamole/config/user-mapping.xml
+curl https://raw.githubusercontent.com/unitehenry/amish/refs/heads/master/guacamole/config/user-mapping.xml -o /var/lib/guacamole/config/user-mapping.xml
 
 # TODO: sed guacamole username / password
 
@@ -69,7 +68,7 @@ docker run -d \
   --name xpod-chromium \
   xpod-chromium
 
-# TODO: Run Agent Browser MCP
+# Run agent browser mcp
 docker run -d \
   --network=amish \
   -e CDP_PORT="http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' xpod-chromium):9222" \
@@ -77,10 +76,10 @@ docker run -d \
   --name agent-browser-mcp \
   agent-browser-mcp
 
-# Install Nginx
+# Install nginx
 sudo apt-get install -y nginx
 
-curl https://github.com/unitehenry/amish/nginx/nginx.conf -o /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/unitehenry/amish/refs/heads/master/nginx/nginx.conf -o /etc/nginx/nginx.conf
 
 # Reload nginx
 sudo systemctl restart nginx
